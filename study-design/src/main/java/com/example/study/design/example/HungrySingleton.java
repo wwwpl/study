99a06aaf35cfc19645609625546fd72e26e8748f
F.W <673391104@qq.com>
2019-05-06 19:47:23 +0800

    新增单例模式的创建

---
+++ study-design/src/main/java/com/example/study/design/example/HungrySingleton.java
@@ -0,0 +1,26 @@
+package com.example.study.design.example;
+
+/**
+ * 饿汉方式创建单例模式
+ *
+ * @author F.W
+ * @date 2019/5/5 16:19
+ */
+public class HungrySingleton {
+
+    private static final HungrySingleton instance = new HungrySingleton();
+
+    /**
+     * 构造方法
+     */
+    private HungrySingleton() {};
+
+    /**
+     * 返回实例
+     * @return
+     */
+    public static HungrySingleton getInstance() {
+        return instance;
+    }
+
+}
