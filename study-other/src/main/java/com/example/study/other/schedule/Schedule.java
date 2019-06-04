b7ce2099730f1b800669a5f4f7e976098a43c045
F.W <673391104@qq.com>
2019-06-04 10:07:10 +0800

    添加定时任务

---
+++ study-other/src/main/java/com/example/study/other/schedule/Schedule.java
@@ -0,0 +1,28 @@
+package com.example.study.other.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author F.W
+ * @date 2019/6/4 10:01
+ */
+@Slf4j
+@Component
+@EnableScheduling
+public class Schedule {
+    @Scheduled(cron = "0/5 * * * * *")
+    public void scheduled(){
+        log.info("=====>>>>>使用cron  {}",System.currentTimeMillis());
+    }
+    @Scheduled(fixedRate = 5000)
+    public void scheduled1() {
+        log.info("=====>>>>>使用fixedRate{}", System.currentTimeMillis());
+    }
+    @Scheduled(fixedDelay = 5000)
+    public void scheduled2() {
+        log.info("=====>>>>>fixedDelay{}",System.currentTimeMillis());
+    }
+}
