9ae3136b9ce665a6c1ab2d09d1b934b76a4935ab
F.W <673391104@qq.com>
2019-06-14 10:18:25 +0800

    修改kafka demo

--- study-queue/src/main/java/com/example/study/queue/controller/CollectController.java
+++ study-queue/src/main/java/com/example/study/queue/controller/CollectController.java
@@ -2,19 +2,14 @@ package com.example.study.queue.controller;
 
 import com.example.study.queue.listen.Listener;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.connector.Response;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
 @RestController
-@RequestMapping("/kafka")
 public class CollectController {
 
     @Autowired
@@ -26,10 +21,10 @@ public class CollectController {
     @GetMapping(value = "/send")
     public void sendKafka() {
         try {
-            kafkaTemplate.send("test", "key", "------------");
-            log.info("发送kafka成功.");
+            kafkaTemplate.send("test", "test", "123321");
+            log.info("发送kafka成功");
         } catch (Exception e) {
-            log.error("发送kafka失败", e);
+            log.error("发送kafka失败:{}", e);
         }
     }
 }
--- study-queue/src/main/java/com/example/study/queue/listen/Listener.java
+++ study-queue/src/main/java/com/example/study/queue/listen/Listener.java
@@ -12,7 +12,7 @@ public class Listener {
 
     @KafkaListener(topics = {"test"})
     public void listen(ConsumerRecord<?, ?> record) {
-        log.info("kafka的key: " + record.key());
-        log.info("kafka的value: " + record.value().toString());
+        log.info("消费kafka的key:{}" ,record.key());
+        log.info("消费kafka的value:{}" , record.value().toString());
     }
 }
