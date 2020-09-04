f8ec493c5c15df5e47a83925e1905cbec3b0b721
F.W <673391104@qq.com>
2020-09-04 18:15:13 +0800

    leet code

---
+++ study-algorithm/src/main/java/com/example/study/arithmetic/leetCode/common/TreeNode.java
@@ -0,0 +1,17 @@
+package com.example.study.arithmetic.leetCode.common;
+
+/**
+ * 二叉树
+ * @author wangfei
+ * @since 2020/04/21
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
